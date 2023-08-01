ab0cd181202e1a4396c468b92f381ced38aed929
Shanaz King <sking1128@YewYew1128.localdomain>
2023-08-01 17:42:02 -0400

    adding py files and templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from surveys import satisfaction_survey 
 
 from flask_debugtoolbar import DebugToolbarExtension
@@ -6,33 +6,37 @@ from flask_debugtoolbar import DebugToolbarExtension
 app = Flask(__name__)
 
 app.config['SECRET_KEY']="catsarecool1234"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-RESPONSES =[]
+RESPONSES = "response"
 
 @app.route('/')
 def show_survey_title():
     """Shows user survey titles available to seleft"""
     return render_template("home.html", satisfaction_survey=satisfaction_survey)
 
+@app.route('/start', methods=['Post'])
+def start_survey():
+    session[RESPONSES] = [] 
+    return redirect('/questions/0')
 
-# @app.route('/questions/0')
-# def show_questions():
-#     """Shows users survey questions to select"""
-    
-#     return render_template("questions.html",question=satisfaction_survey.questions[0])
 
 @app.route('/questions/<int:question_id>')
 def show_questions (question_id):
     """Shows users survey questions to select"""
     if (len(RESPONSES) != question_id):
-        # Trying to access questions out of order.
+        responses = session.get(RESPONSES)
+
+    if (RESPONSES is None):
+        return redirect ('/')
+    
+             
+    if (len(RESPONSES) == len(satisfaction_survey.questions)):       
+        return redirect("/complete")
+    if (len(RESPONSES) != question_id):
         flash("Please answer questions in order.")
         return redirect(f"/questions/{len(RESPONSES)}")
-    if (len(RESPONSES) == len(satisfaction_survey.questions)):
-        # They've answered all the questions! Thank them.
-        return redirect("/complete")
     else:
         return render_template("questions.html",question=satisfaction_survey.questions[question_id])
 
@@ -48,8 +52,9 @@ def handle_question():
     choice = request.form['answer']
 
     # add this response to the session
-   
-    RESPONSES.append(choice)   
+    response=session[RESPONSES]
+    response.append(choice) 
+    session[RESPONSES] = response
     return redirect(f"/questions/{len(RESPONSES)}")
 
 
